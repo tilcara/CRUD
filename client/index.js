acc2e4ef2324b5fe0af22790843a4752b004ca7a
tilcara <enriquesegovianoandroid@gmail.com>
2019-02-14 14:35:11 -0300

    Client fetching express API

--- client/index.js
+++ client/index.js
@@ -1,1 +1,8 @@
-console.log('hello world ');
+
+const API_URL = 'http://localhost:3000/';
+
+fetch(API_URL)
+  .then(res => res.json())
+  .then(result => {
+    console.log(result);
+  });
--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
+const cors = require('cors');
 
 const app = express();
 
@@ -12,6 +12,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.json({
